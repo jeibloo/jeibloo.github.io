7aa3f674bb0049d5d7d1872e2900a9ece4ee30a4
Jason <jasonnova@protonmail.com>
2019-11-12 18:49:13 -0800

    Did some absolutely mind-blowingly horrific javascript to make my vision real.

--- js/power.js
+++ js/power.js
@@ -21,29 +21,75 @@ console.log("+---------------------------+\n| ///////////////////////// |\n+----
 +---------------------------+\n
 */
 
-// Makes things disappear and vertically elongates box.
-var words_id = document.getElementById("words");
-words_id.addEventListener('click', function(event){
+// I don't know how else to toggle things in JS
+var wordsToggle = 0;
+var pictsToggle = 0;
+
+// Makes things disappear and vertically/horizontally elongates box.
+var wordsId = document.getElementById("words");
+var pictsId = document.getElementById("picts");
+
+console.log(wordsId);
+console.log(pictsId);
+
+function removeText() { // This function removes all main screen text
     // Get all the boxes with text.
     var upr = document.getElementById('urighti');
     var upl = document.getElementById('ulefti');
     var dwr = document.getElementById('drighti');
     var dwl = document.getElementById('dlefti');
     // Goes through them and toggle class to hidden.
-    element_list = [upr, upl, dwr, dwl];
-    for (let index = 0; index < element_list.length; index++) {
-        const element = element_list[index];
+    elementList = [upr, upl, dwr, dwl];
+    for (let index = 0; index < elementList.length; index++) {
+        const element = elementList[index];
         element.classList.toggle("hidden");
     }
-    // Make big column full height.
+}
+
+function toggleArrow(wpChoice, triType) {
+    // ------ Reset everything
+    document.getElementById("tri1").innerHTML = "▵";
+    document.getElementById("tri2").innerHTML = "▵";
+
+    if (wpChoice == 0) {
+        wpChoice = 1; // Choice is 1 then make triangle solid
+        document.getElementById(triType).innerHTML = "▴";
+        // Need to return wpChoice so it can register changes
+        return wpChoice;
+    } else {
+        // Turn triangle it back to hollow if already solid
+        wpChoice = 0;
+        document.getElementById(triType).innerHTML = "▵";
+        return wpChoice;
+    }
+}
+
+// VERT
+wordsId.addEventListener('click', function(event){
+    console.log("Here's a 'blog'");
+    // ------ Reset everything
     var bigCol = document.getElementById("bigCol");
-    bigCol.classList.toggle("tall");
-    // And make sure the header is above it and whitish.
     var header = document.getElementById("header");
+    // ------ Reset everything
+    removeText(); // Remove text (duh)
+    console.log(wordsToggle); // debug
+    pictsToggle = 0; // So disgusting I'm sorry
+    wordsToggle = toggleArrow(wordsToggle, "tri1"); // Change arrows to hollow
+    console.log(wordsToggle); // debug
+    bigCol.classList.toggle("tall");
     header.classList.toggle("absolute");
-    // Make sure the bottom button thingies are white too
-    var words = document.getElementById("words");
-    words.classList.toggle("activated");
-    // No idea
-    event.preventDefault();
+    event.preventDefault(); // I don't know what this does
+})
+// HORZ 
+pictsId.addEventListener('click', function(event){
+    console.log("Here's some picts!!!");
+    // ------ Reset everything
+    var bigCol = document.getElementById("bigCol");
+    var header = document.getElementById("header");
+    bigCol.className = "container";
+    header.className = "container";
+    removeText(); // Remove text (duh)
+    wordsToggle = 0;
+    pictsToggle = toggleArrow(pictsToggle, "tri2"); // Change arrows to hollow
+    event.preventDefault(); // I don't know what this does
 })
