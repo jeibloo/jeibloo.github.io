8423b9e3936c3ed55b70f54c2d29b7d03a762eec
Jason <jasonnova@protonmail.com>
2019-11-13 23:08:05 -0800

    Ok got the colour changer to work, phew.

--- js/power.js
+++ js/power.js
@@ -7,6 +7,8 @@ var pictsToggle = 0;
 var bigCol = document.getElementById("bigCol");
 var header = document.getElementById("header");
 var orient = document.getElementById("orient");
+var saying = document.getElementById("saying");
+var uin = document.getElementById("uin");
 
 // Fake cookie, want to do something with this
 window.onload = function uniqueID() {
@@ -14,8 +16,7 @@ window.onload = function uniqueID() {
     // bro these JS slices tho...
     document.getElementById("uin").innerHTML = "UIN - " + uniqueSessionId.slice(-3) + " " + uniqueSessionId.slice(-2);
 }
-
-function removeText(text) { // This function removes all main screen text
+function removeText(text) { // Removes all main screen text
     // Get all the boxes with text.
     let upr = document.getElementById('urighti');
     let upl = document.getElementById('ulefti');
@@ -31,8 +32,7 @@ function removeText(text) { // This function removes all main screen text
     text = document.getElementById(text);
     text.classList.toggle("hidden");
 }
-
-function toggleArrow(wpChoice, triType) {
+function toggleArrow(wpChoice, triType) { // Toggles arrows for the two 'menus'
     // ------ Reset everything
     document.getElementById("tri1").innerHTML = "▵";
     document.getElementById("tri2").innerHTML = "▵";
@@ -49,7 +49,7 @@ function toggleArrow(wpChoice, triType) {
         return wpChoice;
     }
 }
-
+// Calculates and sets the numbers in the header
 function windowCalc(orientWord, toggle, sl1, sl2) {
     let randoN = bigCol.scrollHeight * bigCol.scrollWidth;
     randoN = randoN.toString();
@@ -59,6 +59,53 @@ function windowCalc(orientWord, toggle, sl1, sl2) {
         orient.innerHTML = "NORM: " + randoN.slice(0,6) + "-" + randoN.slice(sl1, sl2) ;
     }
 }
+// Iterate through the collections for a specified class and changes its colour
+function iterateCs(className, color) {
+    Array.from(document.getElementsByClassName(className)).forEach(e => {
+        e.style.color = color;
+    });
+}
+// COLOUR STUFF!!!! WEEEEEE
+var colourId = document.getElementById("colour");
+var colourDict = { //honestly just an excuse to use a dict
+    redy: "#E72D9A",
+    orng: "#FFC132",
+    lime: "#88F230",
+    blak: "#2F353A",
+    blue: "#368AD5"
+}; // Maybe make a hex maker so it's always random?
+var colourList = [];
+var gridList = ['urighti','ulefti','drighti','dlefti'];
+for (const [key, value] of Object.entries(colourDict)) {
+    colourList.push(value);
+}
+colourId.addEventListener('click', function(event) {
+    // Make sure we're switching colours and moving the first
+    // one to the last spot etc etc
+    daColour = colourList.shift()
+    console.log("SWITCHING COLOUR: " + daColour)
+    bigCol.style.backgroundColor = daColour;
+    colourList.push(daColour);
+
+    // Switching when it turns black + going back when it ain't
+    whiteness = "#fcfcfc";
+    blackness = "#2F353A";
+    specialLinks = document.getElementsByClassName("links");
+    if (daColour == blackness) {
+        for(let n of gridList) {
+            document.getElementById(n).style.color = whiteness;
+        }
+        // Special case for those stupid links
+        iterateCs("links",whiteness);
+    } else {
+        // I can do it this way to revert cause I want it all to be black.
+        for(let n of gridList) {
+            document.getElementById(n).style.color = blackness;
+        }
+        // Special case for those stupid links
+        iterateCs("links",blackness);
+    }
+})
 
 // VERT blog
 // Makes things disappear and vertically elongates box.
@@ -74,7 +121,6 @@ wordsId.addEventListener('click', function(event){
     header.classList.toggle("absolute"); // Make sure header stays in place
 
     windowCalc("VERT", wordsToggle, 0, 3);
-    anchorColour();
 
     event.preventDefault(); // I don't know what this does
 })
@@ -99,4 +145,3 @@ pictsId.addEventListener('click', function(event){
 // ----------------------------------------------------------------------------
 // --------------------------------We-go-old-skool-here------------------------
 // ----------------------------------------------------------------------------
-
